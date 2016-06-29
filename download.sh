b93ac6274c3f0c7dc92b75beed5eca7baafad4a8
Rastislav Szabo <raszabo@cisco.com>
2016-06-29 09:20:36 +0200

    add automatic YANG model download script

---
+++ download.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# YANG modules of Unix/Linux applications that use sysrepo as the datastore
+applications=(
+    "https://raw.githubusercontent.com/sysrepo/dnsmasq-sysrepo/master/cfg/dnsmasq%402016-01-22.yang" 
+    "https://raw.githubusercontent.com/sartura/snabb/sysrepo/src/program/lwaftr/loadtest/sysrepo-lwaftr-loadtest%402016-06-09.yang"
+)
+
+
+# YANG modules used internally in sysrepo datastore
+internal=(
+    "https://raw.githubusercontent.com/sysrepo/sysrepo/master/yang/sysrepo-persistent-data.yang"
+)
+
+for i in ${applications[@]}; 
+do
+    wget -P applications/ $i
+done
+
+for i in ${internal[@]};
+do
+    wget -P internal/ $i
+done
+
