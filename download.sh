468bfab490de56a80cf1a88ea368da4bec9f57eb
Rastislav Szabo <raszabo@cisco.com>
2016-07-04 14:20:39 +0200

    dnsmasq YANG module changes

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 # YANG modules of Unix/Linux applications that use sysrepo as the datastore
 applications=(
-    "https://raw.githubusercontent.com/sysrepo/dnsmasq-sysrepo/master/cfg/dnsmasq%402016-01-22.yang" 
+    "https://raw.githubusercontent.com/sysrepo/dnsmasq-sysrepo/master/yang/sysrepo-dnsmasq%402016-01-22.yang" 
     "https://raw.githubusercontent.com/sartura/snabb/sysrepo/src/program/lwaftr/loadtest/sysrepo-lwaftr-loadtest%402016-06-09.yang"
 )
 
