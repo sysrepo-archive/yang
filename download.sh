16e56b224cd8f57f1af7798d93c1629cf6c9e4f6
Rastislav Szabo <raszabo@cisco.com>
2016-07-27 08:19:15 +0200

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,6 +4,8 @@
 applications=(
     "https://raw.githubusercontent.com/sysrepo/dnsmasq-sysrepo/master/yang/sysrepo-dnsmasq%402016-01-22.yang" 
     "https://raw.githubusercontent.com/sartura/snabb/sysrepo/src/program/lwaftr/loadtest/sysrepo-lwaftr-loadtest%402016-06-09.yang"
+    "https://raw.githubusercontent.com/sysrepo/sysrepo-plugin-raspberry/master/yang/sysrepo-raspberry-demo.yang"
+    "https://raw.githubusercontent.com/sysrepo/sysrepo-plugin-kea/master/ietf-kea-dhcpv6%402016-07-16.yang"
 )
 
 
